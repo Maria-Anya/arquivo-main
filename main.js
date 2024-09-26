1eaa8a7af6cde896e9af264c0994c72fc2fca43a
Maria-Anya <134070005+Maria-Anya@users.noreply.github.com>
2024-09-26 11:33:44 -0300

    styst

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function soma(numero1,numero2){
 }
 console.log(soma(1,2));
 
-function soma(numero15,numero6)
-    return numero15+numero6
+function soma(numero1,numero2,numero3,numero4)
+    return numero1+numero2+numero3+numero4
 }
-console.log(soma (15,6))
+console.log(soma (15,6,15,6))
