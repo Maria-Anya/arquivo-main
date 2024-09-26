525d77cf000fd37af7c177df45f32798ee9463d2
Maria-Anya <134070005+Maria-Anya@users.noreply.github.com>
2024-09-26 12:27:28 -0300

    fff

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 alert('MariV. e melina')
 let areaQuadrado = parseInt(prompt('adicione um numero'));
-document.writeareaQuadrado
 
 function soma(numero1,numero2){
     return numero1+numero2;
 }
 console.log(soma(1,2));
 
-function areaQuadrado(lado){
+function areaQuadrado2(lado){
     return lado**2;
 }
-console.log(areaQuadrado (areaQuadrado));
+console.log(areaQuadrado2 (areaQuadrado));
 
 let media = function(numero1,numero2,numero3){
 return numero1+numero2+numero3 ;
-)
+}
