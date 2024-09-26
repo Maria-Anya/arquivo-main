310fe9bd1e6d71ff49b658184b6d52b379d0249e
Maria-Anya <134070005+Maria-Anya@users.noreply.github.com>
2024-09-26 11:59:12 -0300

    u

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-let area Quadrado = parseint(prompt('adicione um numero'));
+let areaQuadrado = parseint(prompt('adicione um numero'));
 
 function soma(numero1,numero2){
     return numero1+numero2;
 }
 console.log(soma(1,2));
 
-function area Quadrado(lado){
-    return lado **2;
+function areaQuadrado(lado){
+    return lado**2;
 }
 console.log(areaQuadrado (15));
 
