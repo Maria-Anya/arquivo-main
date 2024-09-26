f208dbc9489315ad95f16e315b3efd48db88e132
Maria-Anya <134070005+Maria-Anya@users.noreply.github.com>
2024-09-26 11:35:00 -0300

    jbjb

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function soma(numero1,numero2){
 }
 console.log(soma(1,2));
 
-function soma(numero1,numero2,numero3,numero4)
-    return numero1+numero2+numero3+numero4
+function soma(numero1,numero2,numero3,numero4){
+    return numero1+numero2+numero3+numero4;
 }
-console.log(soma (15,6,15,6))
+console.log(soma (15,6,15,6));
