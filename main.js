b140fa2ff9bacc2949b8aa896973685e256520a4
Maria-Anya <134070005+Maria-Anya@users.noreply.github.com>
2024-10-03 11:26:14 -0300

    ddd

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-alert('MariV. e melina')
-let areaQuadrado = parseInt(prompt('adicione um numero'));
+//  alert('MariV. e melina')
+//  let areaQuadrado = parseInt(prompt('adicione um numero'));
+//  function soma(numero1,numero2){
+//      return numero1+numero2;
+//  }
+// console.log(soma(1,2));
 
-function soma(numero1,numero2){
-    return numero1+numero2;
-}
-console.log(soma(1,2));
+//  function areaQuadrado2(lado){
+//      return lado**2;
+//  }
+// console.log(areaQuadrado2 (areaQuadrado));
 
-function areaQuadrado2(lado){
-    return lado**2;
-}
-console.log(areaQuadrado2 (areaQuadrado));
-
-let media = function(numero1,numero2,numero3){
-return numero1+numero2+numero3 ;
-}
+//  let media = function(numero1,numero2,numero3){
+//  return numero1+numero2+numero3 ;
+//  }
+let estados = {AC,AL,AP,AM,BA,CE,DF,ES,GO,MA,MT,MS,MG,PA,PB,PR,PE,PI,RJ,RN,RS,RO,RR,SC,SP,SE,TO}
