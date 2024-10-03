749f8537b327bc7d1c51c49d5e0418c3df621845
Maria-Anya <134070005+Maria-Anya@users.noreply.github.com>
2024-10-03 12:20:54 -0300

    c

--- main.js
+++ main.js
@@ -13,15 +13,16 @@
 //  let media = function(numero1,numero2,numero3){
 //  return numero1+numero2+numero3 ;
 //  }
-let estados = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
-let i = 0;
-while(i <estados.length){
-console.log(i);
-console.log(estados[i]);
-i++;
-}
+// let estados = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
+// let i = 0;
+// while(i <estados.length){
+// console.log(i);
+// console.log(estados[i]);
+// i++;
+// }
 
-for(let j=0;estados.length;j++){
-    console.log(j);
-    console.log(estados[j]);
-}
+// for(let j=0;estados.length;j++){
+//     console.log(j);
+//     console.log(estados[j]);
+// }
+
